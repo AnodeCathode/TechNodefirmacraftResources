a342a290573d8381dc270636a98ccc8891f9923a
AnodeCathode <shotorius_dt@hotmail.com>
2015-08-14 23:14:57 -0600

    2.0 Release

--- src/main/java/zTNFCResources/core/ModDetails.java
+++ src/main/java/zTNFCResources/core/ModDetails.java
@@ -5,9 +5,9 @@ public class ModDetails
 	public static final String ModID = "zTNFCResources";
 	public static final String ModName = "zTNFCResources";
 
-	public static final int VersionMajor = 0;
+	public static final int VersionMajor = 2;
 	public static final int VersionMinor = 0;
-	public static final int VersionRevision = 1;
+	public static final int VersionRevision = 0;
 
 	public static final String ModVersion = VersionMajor + "." + VersionMinor + "." + VersionRevision;
 	public static final String ModDependencies = "";
